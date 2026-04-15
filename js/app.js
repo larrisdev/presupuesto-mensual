de7941b39b1ee0bf953fd2ee602be492ce298586
larrisdev <mcacerez00@gmail.com>
2026-04-14 23:36:21 -0600

    Estructura por componentes y punto de entrada

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+import { inicializar as inicializarResumen } from './componentes/resumen.js';
+import { inicializar as inicializarFormulario } from './componentes/formulario.js';
+import { inicializar as inicializarListado } from './componentes/listado.js';
+
+function arrancar() {
+  inicializarResumen();
+  inicializarFormulario();
+  inicializarListado();
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', arrancar);
+} else {
+  arrancar();
+}
